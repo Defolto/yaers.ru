b8541b0f3a4cf0d67cf75e16e11ae3c3e689d380
Defolto <37343435+Defolto@users.noreply.github.com>
2019-07-21 20:10:32 +0300

    1 page (mobile)

---
+++ MainScript.js
@@ -0,0 +1,10 @@
+if(window.innerWidth < 400){
+    $(".contact").html("Круглосуточно:<br> +7 (4852) 68-53-77<br> +7 (4852) 68-53-75").addClass("text-center");
+}
+if(window.innerWidth < 1000){
+    $("#FooterBorder").css({'border-right' : '0px solid white', 
+    'border-left':'0px solid white',
+    'border-top' : '1px solid white',
+    'border-bottom' : '1px solid white',
+    'margin-top' : '20px'});
+}
---
+++ script.js
@@ -0,0 +1,8 @@
+if(window.innerWidth < 900){
+    $("#block").css({'backgroundColor' : 'rgba(0, 0, 0, 0.399)', 
+    'color':'white', 
+    'min-width':'100%',
+    'padding':'20px',
+    'font-size':'35px', 
+    'font-weight':'700'});
+}
