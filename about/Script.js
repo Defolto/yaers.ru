ef6f7df8b28d871b472b98558f8f9fdd677d129a
Defolto <37343435+Defolto@users.noreply.github.com>
2019-07-24 23:58:20 +0300

    meta

---
+++ about/Script.js
@@ -0,0 +1,12 @@
+if (window.innerWidth < 1100) {
+    $(".padfoto1").removeClass('px-5');
+}
+if (window.innerWidth < 800) {
+    $(".padfoto2").removeClass('px-5');
+}
+if (window.innerWidth < 650) {
+    $(".padfoto3").removeClass('px-5');
+}
+if (window.innerWidth < 450) {
+    $(".padfoto4").removeClass('mx-5').addClass("mx-2");;
+}
